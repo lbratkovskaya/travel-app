e97c2d5325a5e45be6bb669cb1958eebbe5f3a86
lbratkovskaya <lbratkovskaya@naumen.ru>
2021-03-09 21:46:20 +0500

    fix: fix redux router types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,7 +2,9 @@ export interface Country {
   id: string,
   name: string,
   pictureURL: string,
+  capitalLatLng?: [number, number] | undefined,
 }
+
 export interface URLParamTypes {
   countryId: string,
 }
